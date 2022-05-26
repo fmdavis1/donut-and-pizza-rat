e18b341ee5bf867477d4f24d3211adcdedfa62e5
galekwan sango <galekwansango@gmail.com>
2022-05-26 15:51:46 -0400

    filter api data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
+const responseData = fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(err => console.log(err))
@@ -11,10 +11,29 @@ fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
 
  //function Brooklyn
  function brooklynData(){
+    fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=brooklyn')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 
- }
-
+}
 
  bkl.addEventListener('click', brooklynData)
+
+ //fucntion Manhattan
+ function manhattanData(){}
+ man.addEventListener('click', manhattanData)
+
+ //fucntion bronx
+ function bronxData(){}
+ bronx.addEventListener('click', bronxData)
+
+ //fucntion queens
+ function queensData(){}
+ queens.addEventListener('click', queensData)
+
+ //fucntion Staten_island
+ function statenIslandData(){}
+ st.addEventListener('click', statenIslandData)
 
 
