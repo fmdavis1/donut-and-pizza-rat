bb929ea710f720921e3a597b5e7357da29c334f5
Felecia Davis <fmdavis1@gmail.com>
2022-05-26 15:08:56 -0400

    added buttons

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(err => console.log(err))
+
+ const bkl = document.querySelector('#brooklyn')
+ const  man = document.querySelector('#manhattan')
+ const queens = document.querySelector('#queens')
+ const bronx = document.querySelector('#bronx')
+ const st = document.querySelector('#staten-island')
+
+ //function Brooklyn
+ function brooklynData(){
+
+ }
+
+
+ bkl.addEventListener('click', brooklynData)
+
+
